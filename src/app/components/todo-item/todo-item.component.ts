0afed01bcdc9ec9f976d4b61ea09a8fcc43fd31b
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-12-27 09:18:05 -0600

    Update todo-item.component.ts

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -13,6 +13,17 @@ export class TodoItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  }
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-completed': this.todo.completed
+    }
+
+    return classes;
   }
 
 }
