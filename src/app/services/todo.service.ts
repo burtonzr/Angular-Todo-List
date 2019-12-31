78ab2dbd81628d9ac800adc16eb1682e0c955d4c
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2019-12-31 14:58:35 -0600

    Update todo.service.ts

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
+import { Todo } from '../models/Todo';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class TodoService {
+
+  todosUrl:string = 'https://jsonplaceholder.typicode.com/todos';
 
   constructor(private http:HttpClient) { }
 
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo One',
-        completed: true
-      },
-      {
-        id: 2,
-        title: 'Todo Two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo Three',
-        completed: false
-      }
-    ]
+  getTodos():Observable<Todo[]> {
+    return this.http.get<Todo[]>(this.todosUrl); //this.todoUrl is an instance member
   }
 }
