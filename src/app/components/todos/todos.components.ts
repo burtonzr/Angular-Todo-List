cdad51ae93b6331c6ea552758dad3ba818e3d4fb
Zachary Burton <45068978+burtonzr@users.noreply.github.com>
2020-01-01 17:08:09 -0600

    Update todos.components.ts

--- src/app/components/todos/todos.components.ts
+++ src/app/components/todos/todos.components.ts
@@ -20,4 +20,11 @@ export class TodosComponent implements OnInit {
     });
   }
 
+  deleteTodo(todo:Todo) {
+    // Delete from UI
+    this.todos = this.todos.filter(t => t.id !== todo.id);
+    // Delete from server
+    this.todoService.deleteTodo(todo).subscribe();
+  }
+
 }
